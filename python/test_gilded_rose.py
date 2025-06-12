4e00a5e68b0d04b4ba27ad4ad6c96a441d95f7d0
Srini Janarthanam <srinivasancj@gmail.com>
2025-06-12 15:12:43 +0100

    Revert - improved code quality
    This reverts commit 9bd870095eda15005bd56c20f56e0396ad6108c7.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -17,7 +17,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("foo", 5, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must reduce by 1
+        gilded_rose.update_quality() #must reduce by 1
         self.assertEqual(9, items[0].quality)
 
     def test_quality_reduces_by_2_when_sellin_is_0(self):
@@ -26,7 +26,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("foo", 0, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must reduce by 2
+        gilded_rose.update_quality() #must reduce by 2
         self.assertEqual(8, items[0].quality)
 
     def test_quality_reduces_by_2_when_sellin_is_0_2(self):
@@ -35,28 +35,28 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("foo", 1, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must reduce by 1
-        gilded_rose.update_quality()  # must reduce by 2
-        gilded_rose.update_quality()  # must reduce by 2
+        gilded_rose.update_quality() #must reduce by 1
+        gilded_rose.update_quality() #must reduce by 2
+        gilded_rose.update_quality() #must reduce by 2
         self.assertEqual(5, items[0].quality)
-
+     
     def test_quality_never_reduces_below_0(self):
         """
         Test if the quality of an item never goes negative (stays at 0).
         """
         items = [Item("foo", 1, 1)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must reduce by 1
-        gilded_rose.update_quality()  # must reduce by 2 but cant go below 0
+        gilded_rose.update_quality() #must reduce by 1
+        gilded_rose.update_quality() #must reduce by 2 but cant go below 0
         self.assertEqual(0, items[0].quality)
 
     def test_quality_never_goes_above_50(self):
         """
         Test if the quality of an item never goes above 50 (except Sulfuras)
         """
-        items = [Item("foo", 1, 55)]  # item quality could be initiated above 50
+        items = [Item("foo", 1, 55)] #item quality could be initiated above 50
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must reduce by 1 but cant be more than 50
+        gilded_rose.update_quality() #must reduce by 1 but cant be more than 50
         self.assertEqual(49, items[0].quality)
 
     def test_quality_never_goes_above_50_2(self):
@@ -66,17 +66,17 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Aged Brie", 5, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must increase by 1 but not beyond 50
+        gilded_rose.update_quality() #must increase by 1 but not beyond 50
         self.assertEqual(50, items[0].quality)
-
+    
     def test_quality_never_goes_above_50_3(self):
         """
         Test if the quality of an item never goes above 50 (except Sulfuras)
         Test 'Aged Brie' as its quality increases every timestep
         """
-        items = [Item("Aged Brie", 5, 55)]  # item quality could be initiate above 50
+        items = [Item("Aged Brie", 5, 55)] #item quality could be initiate above 50
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must increase by 1 but not beyond 50
+        gilded_rose.update_quality() #must increase by 1 but not beyond 50
         self.assertEqual(50, items[0].quality)
 
     def test_quality_of_sulfuras_is_always_80_1(self):
@@ -85,9 +85,9 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Sulfuras, Hand of Ragnaros", 1, 80)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must stay at 80
+        gilded_rose.update_quality() #must stay at 80
         self.assertEqual(80, items[0].quality)
-
+    
     def test_quality_of_sulfuras_is_always_80_2(self):
         """
         Test if the quality Sulfuras always has quality 80, even if it is initiate with anything other than 80.
@@ -95,7 +95,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Sulfuras, Hand of Ragnaros", 1, 55)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must stay at 80
+        gilded_rose.update_quality() #must stay at 80
         self.assertEqual(80, items[0].quality)
 
     def test_quality_of_sulfuras_is_always_80_3(self):
@@ -105,8 +105,8 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Sulfuras, Hand of Ragnaros", 1, 55)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must stay at 80
-        gilded_rose.update_quality()  # must stay at 80
+        gilded_rose.update_quality() #must stay at 80
+        gilded_rose.update_quality() #must stay at 80
         self.assertEqual(80, items[0].quality)
 
     def test_sellin_date_of_sulfuras_never_goes_down(self):
@@ -115,7 +115,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Sulfuras, Hand of Ragnaros", 1, 55)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must not reduce
+        gilded_rose.update_quality() #must not reduce 
         self.assertEqual(1, items[0].sell_in)
 
     def test_quality_of_aged_brie_goes_up_as_it_gets_old(self):
@@ -124,7 +124,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Aged Brie", 1, 45)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # must increase by 1
+        gilded_rose.update_quality() #must increase by 1
         self.assertEqual(46, items[0].quality)
 
     def test_quality_of_backstage_passes_less_than_10_days_1(self):
@@ -133,7 +133,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 45)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must increase by 2
+        gilded_rose.update_quality() #q must increase by 2
         self.assertEqual(47, items[0].quality)
 
     def test_quality_of_backstage_passes_less_than_10_days_2(self):
@@ -142,16 +142,16 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 45)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must increase by 2
+        gilded_rose.update_quality() #q must increase by 2
         self.assertEqual(47, items[0].quality)
-
+    
     def test_quality_of_backstage_passes_less_than_5_days_1(self):
         """
         Test if ‘Backstage passes’ increases in quality by 3 when sellin date is less than eq 5 days
         """
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must increase by 3
+        gilded_rose.update_quality() #q must increase by 3
         self.assertEqual(48, items[0].quality)
 
     def test_quality_of_backstage_passes_less_than_5_days_2(self):
@@ -160,7 +160,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 45)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must increase by 3
+        gilded_rose.update_quality() #q must increase by 3
         self.assertEqual(48, items[0].quality)
 
     def test_quality_of_backstage_passes_less_than_5_days_6(self):
@@ -169,7 +169,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must go down to 0
+        gilded_rose.update_quality() #q must go down to 0
         self.assertEqual(0, items[0].quality)
 
     def test_quality_of_conjured_items_1(self):
@@ -178,7 +178,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Conjured", 10, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # quality must go down by 2
+        gilded_rose.update_quality() #quality must go down by 2
         self.assertEqual(48, items[0].quality)
 
     def test_quality_of_conjured_items_2(self):
@@ -187,7 +187,7 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Conjured", 0, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must go down by 4
+        gilded_rose.update_quality() #q must go down by 4
         self.assertEqual(46, items[0].quality)
 
     def test_quality_of_conjured_items_3(self):
@@ -196,9 +196,8 @@ class GildedRoseTest(unittest.TestCase):
         """
         items = [Item("Conjured", -1, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()  # q must go down by 4
+        gilded_rose.update_quality() #q must go down by 4
         self.assertEqual(46, items[0].quality)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
